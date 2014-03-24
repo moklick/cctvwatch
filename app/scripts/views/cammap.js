82a6cf7b10e9e4e12f620cd9c2c8b0ef5ed19263
andymensional <andy.bialystock@gmail.com>
2014-03-24 18:17:05 +0100

    made leaflet work ... somehow

--- app/scripts/views/cammap.js
+++ app/scripts/views/cammap.js
@@ -1,11 +1,13 @@
 window.$ = require('jquery');
 window._ = require('lodash');
+window.L = require('leaflet');
 var Backbone = require('backbone');
 Backbone.$ = window.$;
 //Backbone._ = _;
 
 module.exports = Backbone.View.extend({
     template: $('#camMapViewTemplate').html(),
+    className:'global',
     events: {
         'click addCamLink': 'addCamLink'
     },
@@ -13,6 +15,15 @@ module.exports = Backbone.View.extend({
         var tempScript = $('#camMapViewTemplate').html();
         var template = _.template(tempScript);
         this.$el.html(template());
+        console.log(L);
+        console.log(_);
+        window.map = new L.Map($('#map',this.el)[0], {
+        center: new L.LatLng(52.520, 13.385),
+        zoom: 11
+            });
+            var tileLayer = new L.TileLayer('http://{s}.tiles.mapbox.com/v3/moklick.hh7gbc50/{z}/{x}/{y}.png', {
+        attribution: '<a href="http://mapbox.com/about/maps" target="_blank">Mapbox</a> |  Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'
+    }).addTo(map);
         return this;
     }
 
