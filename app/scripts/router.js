6d9144f63a8139fa52a83c214fceb6141b219a7b
andymensional <andy.bialystock@gmail.com>
2014-05-28 19:30:01 +0200

    addcam added

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,18 +1,19 @@
 var MapView = require('./views/map.js'),
     HeaderView = require('./views/header.js'),
-    LoginView = require('./views/login.js');
+    LoginView = require('./views/login.js'),
+    AddCamView = require('./views/addcam.js');
 
 module.exports = Backbone.Router.extend({
+
     routes: {
         '': 'home',
         '!/home': 'home',
-        '!/addCam': 'addCam',
+        '!/addcam': 'addcam',
         '!/login': 'login'
     },
+
     initialize: function () {
-
         this.vent = _.extend({}, Backbone.Events);
-
         new HeaderView({
             el: $('header'),
             vent: this.vent
@@ -21,23 +22,28 @@ module.exports = Backbone.Router.extend({
             el: $('#map'),
             vent: this.vent
         });
-
         this.vent.on('goto', function (data) {
             console.log(data);
             this.navigate('!/' + data, {trigger: true});
         }.bind(this));
     },
+
     home: function () {
+
     },
 
-    addCam: function () {
-        console.log('addCam');
+    addcam: function () {
+        new AddCamView({
+            el: $('#details'),
+            vent: this.vent
+        });
     },
+
     login: function () {
         new LoginView({
-            el: $('#details')//,
-        //    vent: this.vent
+            el: $('#details'),
+            vent: this.vent
         });
-
     }
+
 });
