84b481c6bebf6c49052ea080c49b430a8992811c
moklick <info@moritzklack.com>
2014-05-30 02:54:40 +0200

    fixed login

--- app/scripts/views/login.js
+++ app/scripts/views/login.js
@@ -3,11 +3,17 @@ var BaseView = require('./base.js');
 
 module.exports = BaseView.extend({
 	events: {
-        'click .menu-close': 'closeDetails'   
+        'click .menu-close': 'closeDetails',
+        'click .auth-btn' : 'authRedirect'   
     },
     template: _.template(loginTemplate),
     initialize:function(options){
     	this.vent = options.vent;
+    	_.bindAll(this, 'authRedirect');
         this.renderDetails();
+    },
+    authRedirect: function(evt){
+    	var provider = $(evt.target).attr('data-provider');
+    	location.href = location.origin + '/auth/' + provider;
     }
 });
